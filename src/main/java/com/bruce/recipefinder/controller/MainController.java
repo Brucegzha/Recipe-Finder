e73f04d038a066ffed0203be95b6fa32f282f254
“brucegfz@gmail.com” <brucegfz@gmail.com>
2018-11-05 09:29:16 +1100

    Display fridge item in the table

--- src/main/java/com/bruce/recipefinder/controller/MainController.java
+++ src/main/java/com/bruce/recipefinder/controller/MainController.java
@@ -1,11 +1,34 @@
 package com.bruce.recipefinder.controller;
 
+import com.bruce.recipefinder.entity.Fridge;
+import com.bruce.recipefinder.entity.Recipe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 //import jdk.nashorn.internal.parser.JSONParser;
 
 
 @Controller
 public class MainController {
+    public ArrayList<Fridge> fridgeList = new ArrayList<Fridge>();
+    public ArrayList<Recipe> recipeList = new ArrayList<Recipe>();
 
+    //Home page get fridge, recipes, recommend result
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String getFridgeItemList(Model model) {
+        model.addAttribute("recipeList",recipeList);
+        model.addAttribute("fridgeList",fridgeList);
+        return "index";
+    }
+
+    //Fridge page get fridge list
+    @RequestMapping(value="/fridge",method = RequestMethod.GET)
+    public String getFridgeList(Model model) {
+        model.addAttribute("fridgeList",fridgeList);
+        return "fridge";
+    }
 }
